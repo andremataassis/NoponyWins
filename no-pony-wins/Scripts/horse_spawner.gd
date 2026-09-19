4c8ebeb7c9c4797aee20b2dd4296e5ff92f75fd4
SparTimm <66032192+SparTimm@users.noreply.github.com>
2026-09-19 15:49:40 -0400

    Timm Horse
    Added timm horse model, made timm horse scene, added timm horse to horse pool

--- no-pony-wins/Scripts/horse_spawner.gd
+++ no-pony-wins/Scripts/horse_spawner.gd
@@ -4,12 +4,13 @@ const DEFAULT_HORSE = preload("uid://bxr034afn4d8e")
 const JAMES_BIRD = preload("uid://bhk88s3aucapw")
 const DRAGON_HORSE = preload("uid://kyp588xilrbt")
 const UNICORN = preload("uid://pjbv0asbot3v")
+const TIMM_HORSE = preload("uid://dys2vux5kc3b3")
 
 @export var tracks : Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var horses = [DEFAULT_HORSE, JAMES_BIRD, DRAGON_HORSE, UNICORN]
+	var horses = [DEFAULT_HORSE, JAMES_BIRD, DRAGON_HORSE, UNICORN, TIMM_HORSE]
 	#instantiate horses
 	for i in tracks.get_children():
 		var horse_instance = horses.pick_random().instantiate()
