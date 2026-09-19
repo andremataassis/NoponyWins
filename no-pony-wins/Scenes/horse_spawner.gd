0f200a5e8ebbcd94f8836c220121844871f3e737
Goose1206 <mermaidgirl1000@gmail.com>
2026-09-18 22:57:20 -0400

    horse movement

--- no-pony-wins/Scenes/horse_spawner.gd
+++ no-pony-wins/Scenes/horse_spawner.gd
@@ -1,13 +1,18 @@
 extends Node3D
 
 const DEFAULT_HORSE = preload("uid://bxr034afn4d8e")
+@export var tracks : Node3D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in 8:
+	
+	for i in tracks.get_children():
 		var horse_instance = DEFAULT_HORSE.instantiate()
-		add_child(horse_instance)
-		horse_instance.global_position = global_position + Vector3(0, 0, i * -1);
+		i.add_child(horse_instance)
+		print (str(horse_instance.scale))
+		# this may need to change, horses need to be child-ed under paths
+		#horse_instance.global_position = global_position + Vector3(0, 0, i * -1);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ no-pony-wins/horse_movement.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+@onready var path_follow_3d: PathFollow3D = $"."
+var move_speed = 0.1
+
+# Called when the node enters the scene tree for the first time.
+# idk maybe make them wait for the start of the race here???
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	path_follow_3d.progress_ratio += move_speed * delta
+
+# these guys are called from the crosshair script (to be added)
+func _bomb() -> void:
+	move_speed = 0.001
+
+func _end_bomb() -> void:
+	move_speed = 0.1
+
+func _jetpack() -> void:
+	move_speed = 1.0
+
+func _end_jetpack() -> void:
+	move_speed = .01
