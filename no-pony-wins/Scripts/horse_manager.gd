a9fc79e750979a15d04ba0de06619637d7bb8968
AndreMAssis <andremataassis@gmail.com>
2026-09-20 14:15:30 -0400

    HORSES CAN WIN

--- no-pony-wins/Scripts/horse_manager.gd
+++ no-pony-wins/Scripts/horse_manager.gd
@@ -11,11 +11,16 @@ const DEVEN_HORSE = preload("uid://dv4kb1nbw3d3a")
 const __HORSE = preload("uid://bob0lrymw50b3")
 const TOTALLY_A_HORSE = preload("uid://cpeylqdshx21h")
 
+@export var TOTAL_LAPS: int = 3
 @export var tracks : Node3D
+@export var race_handler : RaceHandler
 var active_horses: Array[Horse] = []
+
+static var inst : HorseManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	inst = self
 	var horses = [DEFAULT_HORSE, JAMES_BIRD, DRAGON_HORSE, UNICORN, TIMM_HORSE, TROPHY_HORSE, DEVEN_HORSE, __HORSE, TOTALLY_A_HORSE]
 	#instantiate horses
 	for i in tracks.get_children():
@@ -23,7 +28,12 @@ func _ready() -> void:
 		var horse_instance = horses[h].instantiate()
 		horses.remove_at(h)
 		i.add_child(horse_instance)
+		horse_instance.set_total_laps(TOTAL_LAPS)
 		active_horses.append(horse_instance)
+
+func win_race(horse: Horse):
+	pause_horses()
+	race_handler.win_race(horse)
 
 func pause_horses():
 	for horse in active_horses:
