e59d52019eeb9ad7454c55468a75da80636aa0db
Goose1206 <mermaidgirl1000@gmail.com>
2026-09-20 11:39:08 -0400

    Totally A Horse joins the battle!

--- no-pony-wins/Scripts/horse_manager.gd
+++ no-pony-wins/Scripts/horse_manager.gd
@@ -9,13 +9,14 @@ const TIMM_HORSE = preload("uid://dys2vux5kc3b3")
 const TROPHY_HORSE = preload("uid://dm5mcf7w6myrw")
 const DEVEN_HORSE = preload("uid://dv4kb1nbw3d3a")
 const __HORSE = preload("uid://bob0lrymw50b3")
+const TOTALLY_A_HORSE = preload("uid://cpeylqdshx21h")
 
 @export var tracks : Node3D
 var active_horses: Array[Horse] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var horses = [DEFAULT_HORSE, JAMES_BIRD, DRAGON_HORSE, UNICORN, TIMM_HORSE, TROPHY_HORSE, DEVEN_HORSE, __HORSE]
+	var horses = [DEFAULT_HORSE, JAMES_BIRD, DRAGON_HORSE, UNICORN, TIMM_HORSE, TROPHY_HORSE, DEVEN_HORSE, __HORSE, TOTALLY_A_HORSE]
 	#instantiate horses
 	for i in tracks.get_children():
 		var h = randi_range(0, horses.size() - 1)
